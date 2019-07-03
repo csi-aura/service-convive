1484dcaee341a9695741460375cb1887a958266a
unknown <crouzicien@gmail.com>
2019-07-03 14:23:01 +0200

    modif uri mongo

--- services/convives.service.js
+++ services/convives.service.js
@@ -3,28 +3,13 @@
 const DbService = require("moleculer-db");
 const MongoAdapter = require("moleculer-db-adapter-mongo");
 
-
-// CONFIGURATION DE MONGODB
-const mongoAdress = process.env.MONGO_URL_CONFIGMAP || 'localhost'
-const mongoPort = process.env.MONGO_PORT_CONFIGMAP || '27017'
-const mongoBase = process.env.MONGO_BASE || 'test'
-const mongoUser = process.env.MONGO_USER_SECRET || ''
-const mongoPass = process.env.MONGO_PASS_SECRET || ''
-const mongoReplicat = process.env.MONGO_RS_CONFIGMAP || ''
-
-const mongoAuth = mongoUser != '' ? `${mongoUser}:${mongoPass}@` : ''
-const mongoRep = mongoReplicat != '' ? `?replicaSet=${mongoReplicat}` : ''
-
-const mongoUrl = `mongodb://${mongoAuth}${mongoAdress}:${mongoPort}/${mongoBase}${mongoRep}`;
-
-
-
+console.log("URI MONGODB => ", process.env.MONGO_URI)
 
 module.exports = {
     name: "convives",
 
     mixins: [DbService],
-    adapter: new MongoAdapter(mongoUrl),
+    adapter: new MongoAdapter(process.env.MONGO_URI),
     collection: "convives",
     actions: {
         create: {
